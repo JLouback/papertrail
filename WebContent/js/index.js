f64a45bdd9ce1d2c2bc9d138f06b104522a3f0fa
JLouback <juliana.m.louback@gmail.com>
2015-11-17 12:31:13 -0500

    Adding skeleton trend analysis component.

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,22 +1,34 @@
+//Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+
 $(document).ready(function() {
 
 	$("#metadata-submit").on("click", function(e) {
 		e.preventDefault();
 		var title = $("#title").val();
+		var keywords = $("#keywords").val();
+		var summary = $("#summary").val();
 		if (validateForm() == true) {
 			// request message on server
-			xhrGet("api/analysis", function(responseText){
-				$("#objective").prop( "disabled", false);
-				$("#methods").prop( "disabled", false);
-				$("#topics").prop( "disabled", false);
-				$("#objective").val(title);
-				$("#methods").val(responseText);
+			xhrPost("api/analysis", title, keywords, summary, function(responseText){
+				var keywordArray = $.parseJSON(responseText);
+				$("#myTabs li").removeClass('disabled');
+				var counter = 1;
+				$.each(keywordArray, function(key, item) {
+					if (item.relevance < 0.6) {
+						return false;
+					}
+					$("#trend-analysis").append(collapsePanel(item.text, item.relevance, counter));
+					drawChart(counter);
+					counter += 1;
+				});
+				$("#trend-tab").tab('show');
 			}, function(err){
 				console.log(err);
 			});
 		}
 	});
-	
+
 	$("#keywords").tokenfield({delimiter: ","});
 
 });
@@ -68,9 +80,13 @@ function createXHR(){
 	return null;
 }
 
-function xhrGet(url, callback, errback){
+function xhrPost(url, title, keywords, summary, callback, errback){
+	var formData = new FormData();
+	formData.append("title", title);
+	formData.append("keywords", keywords);
+	formData.append("summary", summary);
 	var xhr = new createXHR();
-	xhr.open("GET", url, true);
+	xhr.open("POST", url, true);
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if(xhr.status >= 200){
@@ -82,5 +98,48 @@ function xhrGet(url, callback, errback){
 	};
 	xhr.timeout = 3000;
 	xhr.ontimeout = errback;
-	xhr.send();
+	xhr.send(formData);
+}
+
+function collapsePanel(word, relevance, counter) {
+	var html = "<div class=\"panel panel-default\">\n<div class=\"panel-heading\">\n" +
+	"<h4 class=\"panel-title\">\n"+
+	"<a data-toggle=\"collapse\" data-parent=\"#trend-analysis\" href=\"#collapse" + counter + "\">\n" +
+	"<b>" + word + "</b>  |  Relevance score:" + relevance +"</a>\n</h4>\n</div>\n" +
+	"<div id=\"collapse" + counter + "\" class=\"panel-collapse collapse\">" +
+	"<div class=\"panel-body\">Lorem ipsum dolor sit amet, consectetur adipisicing elit," +
+	"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam," +
+	"quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+	"</div>\n</div>\n</div>";
+
+	return html;
+}
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart(counter) {
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Topping');
+	data.addColumn('number', 'Slices');
+	data.addRows([
+	              ['Mushrooms', 3],
+	              ['Onions', 1],
+	              ['Olives', 1],
+	              ['Zucchini', 1],
+	              ['Pepperoni', 2]
+	              ]);
+
+	// Set chart options
+	var options = {'title':'How Much Pizza I Ate Last Night',
+			'width':400,
+			'height':300};
+
+	var id = "collapse" + counter;
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById(id));
+	chart.draw(data, options);
 }
--- src/com/papertrail/Analysis.java
+++ src/com/papertrail/Analysis.java
@@ -1,13 +1,13 @@
 package com.papertrail;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import org.json.JSONArray;
@@ -17,56 +17,49 @@ import org.json.JSONObject;
 
 @Path("/analysis")
 public class Analysis {
+	
+	private String apikey = "";
+	private String urlPattern = "http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords?apikey=%s&text=%s&outputMode=json";
+	
+	private String extractKeywords(String summary) throws IOException {
+		
+		// Create an AlchemyAPI object.
+		summary = summary.replaceAll(" ", "%20");
+		String alchemyUrl = String.format(urlPattern, apikey, summary);
+		System.out.println("URL: " + alchemyUrl);
 
-	@GET
+		URL url = new URL(alchemyUrl);
+		URLConnection connection = url.openConnection();
+
+		JSONArray keywords = null;
+		try (final BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+			String line;
+			StringBuilder response = new StringBuilder();
+			while ((line = in.readLine()) != null) {
+				response.append(line);
+			}
+			final JSONObject json = new JSONObject(response.toString());
+			keywords = json.getJSONArray("keywords");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return keywords.toString();
+	}
+
+	@POST
 	public String analysis(
 			@FormParam("title") String title,
 			@FormParam("keywords") String keywords,
-			@FormParam("summary") String summary) throws SQLException, JSONException {		
-
-		String VCAP_SERVICES = System.getenv("VCAP_SERVICES");
-		JSONObject sqldb;
-		JSONObject credentials;
-		String url = "";
-		String user = "";
-		String pswd = "";
-		if (VCAP_SERVICES != null) {
-			try {
-				JSONObject data = new JSONObject(VCAP_SERVICES);
-				sqldb = (JSONObject) data.get("sqldb");
-				credentials = (JSONObject) sqldb.get("credentials");
-				url = credentials.getString("jdbcurl");
-				user = credentials.getString("username");
-				pswd = credentials.getString("password");
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		Connection conn = null;
+			@FormParam("summary") String summary) {		
+		String response = "error";
 		try {
-			conn = DriverManager.getConnection(url, user, pswd);
-			conn.setAutoCommit(false);
-		} catch (SQLException e) {
+			response = extractKeywords(summary);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		String statement = "SELECT \"authors\",\"categories\",\"title\"" 
-				+ " FROM \"USER09041\".\"abstracts\""
-				+ " WHERE \"categories\" LIKE '%cs.CV%'";
-		Statement stmt = conn.createStatement();
-		stmt.execute(statement);
-		ResultSet rs = stmt.getResultSet();
-
-		JSONArray data = new JSONArray();
-		while(rs.next() ) {
-			JSONObject row = new JSONObject();
-			JSONArray authors = new JSONArray(rs.getString(1));
-			JSONObject author = (JSONObject) authors.get(0);
-			row.put("authors", author.getString("fullname"));
-			row.put("categories", rs.getString(2));
-			row.put("title", rs.getString(3));
-			data.put(row);
 		}
-		return data.toString();
+		return response;
 	}
+
 }
